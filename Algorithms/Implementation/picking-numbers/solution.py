4527ca8eecb4ca4935c08090a7392c0169086178
Traction <19631364+k3rn31p4nic@users.noreply.github.com>
2019-06-04 18:07:02 +0530

    add Picking Numbers solution

---
+++ Algorithms/Implementation/picking-numbers/solution.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import os
+
+#
+# Complete the 'pickingNumbers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+def pickingNumbers(a):
+    # Write your code here
+    return max(a.count(n) + a.count(n + 1) for n in set(a))
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = pickingNumbers(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
