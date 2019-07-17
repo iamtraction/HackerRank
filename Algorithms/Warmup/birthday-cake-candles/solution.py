6840be302a66755fa8cac3ffce1de0c2d9ffe441
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-07-17 07:24:14 +0530

    Add Birthday Cake Candles

---
+++ Algorithms/Warmup/birthday-cake-candles/solution.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import os
+
+# Complete the birthdayCakeCandles function below.
+def birthdayCakeCandles(ar):
+    return ar.count(max(ar))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
