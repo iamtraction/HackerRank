66b9b5b3c840273f3d0aae564c74b072ac649b6b
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-07-17 07:01:22 +0530

    Add A Very Big Sum

---
+++ Algorithms/Warmup/a-very-big-sum/solution.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import os
+
+# Complete the aVeryBigSum function below.
+def aVeryBigSum(ar):
+    return sum(i for i in ar)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
