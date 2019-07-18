29cc4d24988ef10cef9368ca8157d0e547f0dfc1
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-07-18 23:24:29 +0530

    Add solution for Service Lane

---
+++ Algorithms/Implementation/service-lane/solution.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import os
+
+# Complete the serviceLane function below.
+def serviceLane(width, cases):
+    return [min(width[case[0]:case[1] + 1]) for case in cases]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nt = input().split()
+
+    n = int(nt[0])
+
+    t = int(nt[1])
+
+    width = list(map(int, input().rstrip().split()))
+
+    cases = []
+
+    for _ in range(t):
+        cases.append(list(map(int, input().rstrip().split())))
+
+    result = serviceLane(width, cases)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
