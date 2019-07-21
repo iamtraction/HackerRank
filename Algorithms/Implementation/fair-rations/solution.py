60a878f4d1d1ad3534a2beda5580ae21e94baf3a
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-07-21 18:47:40 +0530

    Add solution for Fair Rations

---
+++ Algorithms/Implementation/fair-rations/solution.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import os
+
+# Complete the fairRations function below.
+def fairRations(B):
+    carry = 0
+    carries = 0
+
+    for b in B:
+        carry = (carry + b) % 2
+        carries += carry
+
+    return "NO" if carry else carries * 2
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    N = int(input())
+
+    B = list(map(int, input().rstrip().split()))
+
+    result = fairRations(B)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
