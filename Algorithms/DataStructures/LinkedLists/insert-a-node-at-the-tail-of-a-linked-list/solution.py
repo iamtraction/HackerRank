f2477bfe5e3153dead949ce4c3d34c5488a8d0cd
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-08-04 01:31:43 +0530

    Add solution for Insert a Node at the Tail of a Linked List

---
+++ Algorithms/DataStructures/LinkedLists/insert-a-node-at-the-tail-of-a-linked-list/solution.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import os
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the insertNodeAtTail function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def insertNodeAtTail(head, data):
+    node = SinglyLinkedListNode(data)
+
+    if head:
+        current = head
+        while current.next:
+            current = current.next
+        current.next = node
+    else:
+        head = node
+    
+    return head
+
+if __name__ == '__main__':
