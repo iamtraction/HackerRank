9144a455c45ab296d310d5451acde3e72d0e3dc1
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2019-08-07 21:20:58 +0530

    Add solution for Inserting a Node Into a Sorted Doubly Linked List

---
+++ DataStructures/LinkedLists/insert-a-node-into-a-sorted-doubly-linked-list/solution.py
@@ -0,0 +1,86 @@
+#!/bin/python3
+
+import os
+
+class DoublyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = DoublyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+
+
+        self.tail = node
+
+def print_doubly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the sortedInsert function below.
+
+#
+# For your reference:
+#
+# DoublyLinkedListNode:
+#     int data
+#     DoublyLinkedListNode next
+#     DoublyLinkedListNode prev
+#
+#
+def sortedInsert(head, data):
+    if head is None:
+        head = DoublyLinkedListNode(data)
+    elif head.data > data:
+        node = DoublyLinkedListNode(data)
+
+        node.next = head
+        node.prev = head.prev
+
+        head.prev = node
+        head = node
+    elif head.data <= data:
+        head.next = sortedInsert(head.next, data)
+        head.next.prev = head
+
+    return head
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        llist_count = int(input())
+
+        llist = DoublyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        data = int(input())
+
+        llist1 = sortedInsert(llist.head, data)
+
+        print_doubly_linked_list(llist1, ' ', fptr)
+        fptr.write('\n')
+
+    fptr.close()
